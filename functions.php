8cd2acd9a77dd8c5cd3959e70e2d9c21535db97e
Sudip-Bhowmick <writetosb786@gmail.com>
2024-12-03 10:01:57 +0530

    added post-thumbnails

--- functions.php
+++ functions.php
@@ -20,6 +20,12 @@ function my_custom_theme_setup() {
 }
 add_action('after_setup_theme', 'my_custom_theme_setup');
 
+// featured images
+function my_theme_setup(){
+	add_theme_support('post-thumbnails');
+}
+add_action('after_setup_theme', 'my_theme_setup');
+
 
 // global files
 function enqueue_global_styles() {
