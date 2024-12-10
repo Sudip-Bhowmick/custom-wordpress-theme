090a210e2b6d44d5f238455b8fe33a27ac208364
Sudip-Bhowmick <writetosb786@gmail.com>
2024-12-10 13:56:02 +0530

    added header code

--- js/global.js
+++ js/global.js
@@ -1,12 +1,44 @@
-const lenis = new Lenis()
+const lenis = new Lenis();
 
-lenis.on('scroll', (e) => {
-//   console.log(e)
-})
+lenis.on("scroll", (e) => {
+  //   console.log(e)
+});
 
 function raf(time) {
-  lenis.raf(time)
-  requestAnimationFrame(raf)
+  lenis.raf(time);
+  requestAnimationFrame(raf);
 }
 
-requestAnimationFrame(raf)
+requestAnimationFrame(raf);
+
+// header
+document.addEventListener("DOMContentLoaded", (event) => {
+  document
+    .querySelector(".hfe-nav-menu__toggle.hfe-flyout-trigger")
+    .addEventListener("click", function () {
+      const overlay = document.querySelector(".hfe-flyout-overlay");
+      const side = document.querySelector(".hfe-side");
+      const ham = document.querySelector(
+        ".hfe-nav-menu__toggle.hfe-flyout-trigger"
+      );
+
+      if (overlay.style.display === "none" || overlay.style.display === "") {
+        overlay.style.display = "block";
+        overlay.style.opacity = 0;
+        setTimeout(() => {
+          overlay.style.opacity = 1;
+        }, 10);
+      } else {
+        overlay.style.opacity = 0;
+        overlay.addEventListener(
+          "transitionend",
+          function () {
+            overlay.style.display = "none";
+          },
+          { once: true }
+        );
+      }
+      side.classList.toggle("visible");
+      ham.classList.toggle("visible");
+    });
+});
